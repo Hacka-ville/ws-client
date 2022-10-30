6be899dd5b6b6ce42fe82b6e0c315c083d13d114
sergiu <sergiu-nicolas.stanciu@s.unibuc.ro>
2022-10-30 19:02:28 +0200

    change request

--- src/components/CreateTravelComponents/flights/Flights.js
+++ src/components/CreateTravelComponents/flights/Flights.js
@@ -29,7 +29,7 @@ const Flights = (props) => {
                 },
                 body: JSON.stringify({
                     startDate: `${startDate.getFullYear()}-${parseInt(startDate.getMonth(), 10)> 9 ? startDate.getMonth() : '0' + startDate.getMonth()}-${parseInt(startDate.getDate(), 10) > 9 ? startDate.getDate() : '0' + startDate.getDate()}`,
-                    endDate: `${endDate.getFullYear()}-${endDate.getMonth()}-${endDate.getDay()}`,
+                    endDate: `${endDate.getFullYear()}-${parseInt(endDate.getMonth(), 10)> 9 ? endDate.getMonth() : '0' + endDate.getMonth()}-${parseInt(endDate.getDate(), 10)> 9 ? endDate.getDate() : '0' + endDate.getDate()}`,
                     location: {
                         flightLocation: cityName,
                         arriveLocation: currentCity
